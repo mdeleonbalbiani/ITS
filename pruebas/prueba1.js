0d6268f60ad83789185db4aed70d79baf71c37fd
mdeleonbalbiani <martinadeleonbalbiani@gmail.com>
2021-08-03 12:57:18 -0300

    Commiteo de pruebas

--- pruebas/prueba1.js
+++ pruebas/prueba1.js
@@ -1,3 +1,108 @@
+//------ VALIDACIONES ------------
+const form = document.getElementById('user-form');
+const submitButton = document.getElementById('submit-btn');
+
+let timeout = null;
+  //Objeto para validacion de errores en input y usarlo para habilitación de boton
+let errors = {
+    primerEscrito: true,
+    primerParcial: true,
+    segundoEscrito: true,
+    segundoParcial: true,
+    tercerEscrito: true,
+    faltas: true,
+}
+
+document.querySelectorAll('.form-box').forEach((box) => {
+  const boxInput = box.querySelector('input');
+
+  boxInput.addEventListener('keydown', () => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      console.log(`Input: ${boxInput.name} value: `, boxInput.value);
+      validation(box, boxInput);
+    }, 300);
+  });
+});
+
+ //Funcion para validacion de inputs
+validation = (box, boxInput) => {
+
+    if (boxInput.name == 'primerEscrito') {
+        if ((boxInput.value < 0) || (boxInput.value > 12) || (isNaN(boxInput.value)) || (boxInput.value==="")) {
+            showError(true, box, boxInput);
+          } else {
+            showError(false, box, boxInput);
+          }
+    }
+
+    if (boxInput.name == 'primerParcial') {
+        if ((boxInput.value < 0) || (boxInput.value > 12) || (isNaN(boxInput.value)) || (boxInput.value==="")) {
+            showError(true, box, boxInput);
+          } else {
+            showError(false, box, boxInput);
+          }
+    }
+
+    if (boxInput.name == 'segundoEscrito') {
+        if ((boxInput.value < 0) || (boxInput.value > 12) || (isNaN(boxInput.value)) || (boxInput.value==="")) {
+            showError(true, box, boxInput);
+          } else {
+            showError(false, box, boxInput);
+          }
+    }
+
+    if (boxInput.name == 'segundoParcial') {
+        if ((boxInput.value < 0) || (boxInput.value > 12) || (isNaN(boxInput.value)) || (boxInput.value==="")) {
+            showError(true, box, boxInput);
+          } else {
+            showError(false, box, boxInput);
+          }
+    }
+
+    if (boxInput.name == 'tercerEscrito') {
+        if ((boxInput.value < 0) || (boxInput.value > 12) || (isNaN(boxInput.value)) || (boxInput.value==="")) {
+            showError(true, box, boxInput);
+          } else {
+            showError(false, box, boxInput);
+          }
+    }
+
+    if (boxInput.name == 'faltas') {
+        if ((boxInput.value < 0) || (isNaN(boxInput.value)) || (boxInput.value==="")) {
+            showError(true, box, boxInput);
+          } else {
+            showError(false, box, boxInput);
+          }
+    }
+ //Llamada de la funcion para habilitar/deshabilitar boton
+ submitController();
+};
+
+//Funcion para agregar clases de error o success
+showError = (check, box, boxInput) => {
+  if (check) {
+    box.classList.remove('form-success');
+    box.classList.add('form-error');
+    errors[boxInput.name] = true;
+  } else {
+    box.classList.remove('form-error');
+    box.classList.add('form-success');
+    errors[boxInput.name] = false;
+  }
+};
+
+//Funcion para habilitar o deshabilitar el boton
+function submitController(){
+    if (errors.primerEscrito || errors.primerParcial || errors.segundoEscrito || errors.segundoParcial || errors.tercerEscrito || errors.faltas) {
+        submitButton.toggleAttribute('disabled', true)
+    } else {
+        submitButton.toggleAttribute('disabled', false)
+    }
+}
+
+
+
 // SELECCION DE DATOS
 
 let seleccion = document.getElementById('seleccion');
@@ -6,63 +111,63 @@ seleccion.addEventListener('change', (event) => {
     let text = seleccion.options[seleccion.selectedIndex].innerText;
     switch (event.target.value) {
         case "1":
-            //mostrarForm(true);
+            mostrarForm(true);
             sessionStorage.setItem('Nombre', text);
             break;
         case "2":
-            //mostrarForm(true);
+            mostrarForm(true);
             sessionStorage.setItem('Nombre', text);
             break;
         case "3":
-           // mostrarForm(true);
+            mostrarForm(true);
             sessionStorage.setItem('Nombre', text);
             break;
         case "4":
-            //mostrarForm(true);
+            mostrarForm(true);
             sessionStorage.setItem('Nombre', text);
             break;
         case "5":
-            //mostrarForm(true);
+            mostrarForm(true);
             sessionStorage.setItem('Nombre', text);
             break;
         case "6":
-            //mostrarForm(true);
+            mostrarForm(true);
             sessionStorage.setItem('Nombre', text);
             break;
         case "7":
-            //mostrarForm(true);
+            mostrarForm(true);
             sessionStorage.setItem('Nombre', text);
             break;
         case "8":
-            //mostrarForm(true);
+            mostrarForm(true);
             sessionStorage.setItem('Nombre', text);
             break;
         case "9":
-            //mostrarForm(true);
+            mostrarForm(true);
             sessionStorage.setItem('Nombre', text);
             break;
         case "10":
-            //mostrarForm(true);
+            mostrarForm(true);
             sessionStorage.setItem('Nombre', text);
             break;
         case "11":
-            //mostrarForm(true);
+            mostrarForm(true);
             sessionStorage.setItem('Nombre', text);
             break;
         default:
-            //mostrarForm(false);
+            mostrarForm(false);
             break;
     }
 });
 
-/* function mostrarForm(boolean) {
+function mostrarForm(boolean) {
     if (boolean) {
         form.classList.remove('formHidden');
     }
     else{
         form.classList.add('formHidden');
     }
-} */
+}
 
 // ENTIDAD
 
