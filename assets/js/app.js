fe14b512c6e5b97e181ef8ac230f07f9ae3cb723
mdeleonbalbiani <martinadeleonbalbiani@gmail.com>
2021-08-26 11:51:12 -0300

    Commiteo entrega final

--- assets/js/app.js
+++ assets/js/app.js
@@ -117,9 +117,10 @@ function guardarDatos(e){
     
 }
 
-function filterGrupos(){
-    
+function filterArray(){
     listadoAlumnos = JSON.parse(localStorage.getItem("alumnos"));
+
+    /* -- FILTRADO DE GRUPOS --  */
 
     let listaGrupo1 = listadoAlumnos.filter(function (element) {
         return (element.idGrupo === 1);
@@ -137,42 +138,111 @@ function filterGrupos(){
         return (element.idGrupo === 4);
     });
 
-    console.log(listaGrupo1);
+    /* console.log(listaGrupo1);
     console.log(listaGrupo2);
     console.log(listaGrupo3);
-    console.log(listaGrupo4);
+    console.log(listaGrupo4); */
+
+    /* -- FILTRADO DE ALUMNOS -- */
+
+    /* listaGrupo1.forEach(element => {
+        alumnoGrupo1 =  Math.max(Math, element.promedioFinal);
+        
+    });
+   
+    console.log(typeof alumnoGrupo1); */
+    /* console.log(typeof alumnoGrupo2);
+    console.log(typeof alumnoGrupo3);
+    console.log(typeof alumnoGrupo4); */
+
+
+    /* -- FILTRADO DE APROBADOS -- */
+
+    let aprobadosGrupo1 = 0;
+    let desaprobadosGrupo1 = 0;
+
+    listaGrupo1.forEach(element => {
+        if (element.promedioFinal >= 7) {
+            aprobadosGrupo1 += 1;
+        }
+        else{
+            desaprobadosGrupo1 += 1;
+        }
+     });
+
+    let aprobadosGrupo2 = 0;
+    let desaprobadosGrupo2 = 0;
+    listaGrupo2.forEach(element => {
+        if (element.promedioFinal >= 7) {
+            aprobadosGrupo2 += 1;
+        }
+        else{
+            desaprobadosGrupo2 += 1;
+        }
+     });
+
+    let aprobadosGrupo3 = 0;
+    let desaprobadosGrupo3 = 0;
+    listaGrupo3.forEach(element => {
+        if (element.promedioFinal >= 7) {
+            aprobadosGrupo3 += 1;
+        }
+        else{
+            desaprobadosGrupo3 += 1;
+        }
+     });
+
+    let aprobadosGrupo4 = 0;
+    let desaprobadosGrupo4 = 0;
+    listaGrupo4.forEach(element => {
+        if (element.promedioFinal >= 7) {
+            aprobadosGrupo4 += 1;
+        }
+        else{
+            desaprobadosGrupo4 += 1;
+        }
+     });
+
+
+    /* -- LLAMADA DE FUNCIONES MOSTRAR -- */
 
     let grupo = identificaciónDeGrupo();
     switch (grupo) {
         case 1:
-            mostrar(listaGrupo1);
+            mostrarPromedios(listaGrupo1);
+            /* mostrarAlumno(alumnoGrupo1); */
+            mostrarGrafica(aprobadosGrupo1, desaprobadosGrupo1)
             break;
         case 2:
-            mostrar(listaGrupo2);
+            mostrarPromedios(listaGrupo2);
+            /* mostrarAlumno(alumnoGrupo2); */
+            mostrarGrafica(aprobadosGrupo2, desaprobadosGrupo2)
             break;
         case 3:
-            mostrar(listaGrupo3);
+            mostrarPromedios(listaGrupo3);
+            /* mostrarAlumno(alumnoGrupo3); */
+            mostrarGrafica(aprobadosGrupo3, desaprobadosGrupo3)
             break;
         case 4:
-            mostrar(listaGrupo4);
+            mostrarPromedios(listaGrupo4);
+            /* mostrarAlumno(alumnoGrupo4); */
+            mostrarGrafica(aprobadosGrupo4, desaprobadosGrupo4)
             break;
         default:
             break;
     }
-
 }
 
-function mostrar(array){
-
-    $("#imprimir").empty();
+function mostrarPromedios(array){
+    $("#imprimirPromedios").empty();
     if (array != null) {
         array.forEach(element => {
-            $("#imprimir").append(`
+            $("#imprimirPromedios").append(`
             <div class="card col-sm-3 m-2 cardResultado" style="width: 13rem;">
                 <div class="card-body">
-                    <h5 class="card-title text-center">${element.nombre}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted text-center">Promedio: ${element.promedioFinal}</h6>
-                    <p class="card-text text-center">${element.devolucionFinal}</p>
+                    <h5 class="card-title text-center text-white">${element.nombre}</h5>
+                    <h6 class="card-subtitle mb- text-center text-warning">Promedio: ${element.promedioFinal}</h6>
+                    <p class="card-text text-center text-white">${element.devolucionFinal}</p>
                 </div>
             </div>
             `)
@@ -180,12 +250,48 @@ function mostrar(array){
     }
     else{
         console.log("El array esta vacio");
+    }   
+}
+
+function mostrarAlumno(array) {
+    $("#imprimirAlumno").empty();
+    if (array != null) {
+        array.forEach(element => {
+            $("#imprimirAlumno").append(`
+            <div class="card col-sm-3 m-2 cardResultado" style="width: 13rem;">
+                <div class="card-body">
+                    <h5 class="card-title text-center text-white">${element.nombre}</h5>
+                    <h6 class="card-subtitle mb- text-center text-warning">Promedio: ${element.promedioFinal}</h6>
+                    <p class="card-text text-center text-white">${element.devolucionFinal}</p>
+                </div>
+            </div>
+            `)
+        });
     }
-        
+    else{
+        console.log("El array esta vacio");
+    } 
+}
+
+function mostrarGrafica(nroAprobados, nroDesaprobados) {
+    let grafica = $("#grafica");
+    let xValues = ["Aprobados", "Desaprobados"];
+    let yValues = [nroAprobados, nroDesaprobados];
+    let barColors = ["green", "red"];
+
+    let newChart = new Chart(grafica, {
+    type: "doughnut",
+    data: {
+        labels: xValues,
+        datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+        }]
     }
+    });
+}
     
 
 // EVENTOS
 btnGuardar.addEventListener("click", guardarDatos);
-$(".resultados").click(filterGrupos)
-//filterGrupos();
+$(".resultados").click(filterArray);
