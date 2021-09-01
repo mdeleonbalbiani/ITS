523f3ae657af146a753a53bece60c5d6495293d1
mdeleonbalbiani <martinadeleonbalbiani@gmail.com>
2021-08-31 21:07:12 -0300

    Commiteo entrega final

--- assets/js/app.js
+++ assets/js/app.js
@@ -121,133 +121,154 @@ function guardarDatos(e){
     
 }
 
-function filterArray(){
-    /* $("#imprimirGrafica").empty(); */
+/*Se filtra el array de todos los alumnos y se crea un nuevo array para cada grupo */
+function filtrarArrayPorGrupos() {
     listadoAlumnos = JSON.parse(localStorage.getItem("alumnos"));
-
     if (listadoAlumnos != null) {
-/* -- FILTRADO DE GRUPOS --  */
+        const listaGrupo1 = listadoAlumnos.filter(function (element) {
+            return (element.idGrupo === 1);
+        });
+        const listaGrupo2 = listadoAlumnos.filter(function (element) {
+            return (element.idGrupo === 2);
+        });
+        const listaGrupo3 = listadoAlumnos.filter(function (element) {
+            return (element.idGrupo === 3);
+        });
+        const listaGrupo4 = listadoAlumnos.filter(function (element) {
+            return (element.idGrupo === 4);
+        });
+        sessionStorage.setItem('Lista Grupo 1', JSON.stringify(listaGrupo1));
+        sessionStorage.setItem('Lista Grupo 2', JSON.stringify(listaGrupo2));
+        sessionStorage.setItem('Lista Grupo 3', JSON.stringify(listaGrupo3));
+        sessionStorage.setItem('Lista Grupo 4', JSON.stringify(listaGrupo4));
 
-let listaGrupo1 = listadoAlumnos.filter(function (element) {
-    return (element.idGrupo === 1);
-});
-
-let listaGrupo2 = listadoAlumnos.filter(function (element) {
-    return (element.idGrupo === 2);
-});
-
-let listaGrupo3 = listadoAlumnos.filter(function (element) {
-    return (element.idGrupo === 3);
-});
-
-let listaGrupo4 = listadoAlumnos.filter(function (element) {
-    return (element.idGrupo === 4);
-});
-
-/* console.log(listaGrupo1);
-console.log(listaGrupo2);
-console.log(listaGrupo3);
-console.log(listaGrupo4); */
-
-/* -- FILTRADO DE ALUMNOS -- */
-
-let mayoresNotasGrupo1 = listaGrupo1.sort(((a, b) => b.promedioFinal - a.promedioFinal));
-let alumnoGrupo1 = mayoresNotasGrupo1[0];
-
-let mayoresNotasGrupo2 = listaGrupo2.sort(((a, b) => b.promedioFinal - a.promedioFinal));
-let alumnoGrupo2 = mayoresNotasGrupo2[0];
-
-let mayoresNotasGrupo3 = listaGrupo3.sort(((a, b) => b.promedioFinal - a.promedioFinal));
-let alumnoGrupo3 = mayoresNotasGrupo3[0];
-
-let mayoresNotasGrupo4 = listaGrupo4.sort(((a, b) => b.promedioFinal - a.promedioFinal));
-let alumnoGrupo4 = mayoresNotasGrupo4[0];
-
-/* console.log(alumnoGrupo1);
-console.log(alumnoGrupo2);
-console.log(alumnoGrupo3);
-console.log(alumnoGrupo4); */
-
-
-/* -- FILTRADO DE APROBADOS -- */
-
-let aprobadosGrupo1 = 0;
-let desaprobadosGrupo1 = 0;
-
-listaGrupo1.forEach(element => {
-    if (element.promedioFinal >= 7) {
-        aprobadosGrupo1 += 1;
-    }
-    else{
-        desaprobadosGrupo1 += 1;
-    }
- });
-
-let aprobadosGrupo2 = 0;
-let desaprobadosGrupo2 = 0;
-listaGrupo2.forEach(element => {
-    if (element.promedioFinal >= 7) {
-        aprobadosGrupo2 += 1;
-    }
-    else{
-        desaprobadosGrupo2 += 1;
-    }
- });
-
-let aprobadosGrupo3 = 0;
-let desaprobadosGrupo3 = 0;
-listaGrupo3.forEach(element => {
-    if (element.promedioFinal >= 7) {
-        aprobadosGrupo3 += 1;
-    }
-    else{
-        desaprobadosGrupo3 += 1;
-    }
- });
-
-let aprobadosGrupo4 = 0;
-let desaprobadosGrupo4 = 0;
-listaGrupo4.forEach(element => {
-    if (element.promedioFinal >= 7) {
-        aprobadosGrupo4 += 1;
-    }
-    else{
-        desaprobadosGrupo4 += 1;
-    }
- });
-
-
-/* -- LLAMADA DE FUNCIONES MOSTRAR -- */
-
-let grupo = identificaciónDeGrupo();
-switch (grupo) {
-    case 1:
-        mostrarPromedios(listaGrupo1);
-        mostrarAlumno(alumnoGrupo1);
-        mostrarGrafica(aprobadosGrupo1, desaprobadosGrupo1)
-        break;
-    case 2:
-        mostrarPromedios(listaGrupo2);
-        mostrarAlumno(alumnoGrupo2);
-        mostrarGrafica(aprobadosGrupo2, desaprobadosGrupo2)
-        break;
-    case 3:
-        mostrarPromedios(listaGrupo3);
-        mostrarAlumno(alumnoGrupo3);
-        mostrarGrafica(aprobadosGrupo3, desaprobadosGrupo3)
-        break;
-    case 4:
-        mostrarPromedios(listaGrupo4);
-        mostrarAlumno(alumnoGrupo4);
-        mostrarGrafica(aprobadosGrupo4, desaprobadosGrupo4)
-        break;
-    default:
-        break;
-}
-    } else {
+        let grupo = identificaciónDeGrupo();
+        switch (grupo) {
+            case 1:
+                mostrarPromedios(listaGrupo1);
+                break;
+            case 2:
+                mostrarPromedios(listaGrupo2);
+                break;
+            case 3:
+                mostrarPromedios(listaGrupo3);
+                break;
+            case 4:
+                mostrarPromedios(listaGrupo4);
+                break;
+            default:
+                break;
+        }
+    } 
+    else {
         console.log("No hay ingresos de alumnos");
     }
-    
+}
+/*Se filtra el array de todos los grupos y se crean un nuevos arrays con los alumnos con mayor nota por grupo */
+function filtrarArrayMejorAlumno() {
+    listaGrupo1 = JSON.parse(sessionStorage.getItem('Lista Grupo 1'));
+    listaGrupo2 = JSON.parse(sessionStorage.getItem('Lista Grupo 2'));
+    listaGrupo3 = JSON.parse(sessionStorage.getItem('Lista Grupo 3'));
+    listaGrupo4 = JSON.parse(sessionStorage.getItem('Lista Grupo 4'));
+
+    let mayoresNotasGrupo1 = listaGrupo1.sort(((a, b) => b.promedioFinal - a.promedioFinal));
+    let alumnoGrupo1 = mayoresNotasGrupo1[0];
+
+    let mayoresNotasGrupo2 = listaGrupo2.sort(((a, b) => b.promedioFinal - a.promedioFinal));
+    let alumnoGrupo2 = mayoresNotasGrupo2[0];
+
+    let mayoresNotasGrupo3 = listaGrupo3.sort(((a, b) => b.promedioFinal - a.promedioFinal));
+    let alumnoGrupo3 = mayoresNotasGrupo3[0];
+
+    let mayoresNotasGrupo4 = listaGrupo4.sort(((a, b) => b.promedioFinal - a.promedioFinal));
+    let alumnoGrupo4 = mayoresNotasGrupo4[0];
+
+    let grupo = identificaciónDeGrupo();
+    switch (grupo) {
+        case 1:
+            mostrarAlumno(alumnoGrupo1);
+            break;
+        case 2:
+            mostrarAlumno(alumnoGrupo2);
+            break;
+        case 3:
+            mostrarAlumno(alumnoGrupo3);
+            break;
+        case 4:
+            mostrarAlumno(alumnoGrupo4);
+            break;
+        default:
+            break;
+    }
+}
+/*Se filtra el array de todos los grupos y se crean dos nuevos array, los alumnos aprobados y los alumnos desaprobados */
+function filtrarArrayPorAprobacion(){
+    let listaGrupo1 = JSON.parse(sessionStorage.getItem('Lista Grupo 1'));
+    let listaGrupo2 = JSON.parse(sessionStorage.getItem('Lista Grupo 2'));
+    let listaGrupo3 = JSON.parse(sessionStorage.getItem('Lista Grupo 3'));
+    let listaGrupo4 = JSON.parse(sessionStorage.getItem('Lista Grupo 4'));
+
+    let aprobadosGrupo1 = 0;
+    let desaprobadosGrupo1 = 0;
+    listaGrupo1.forEach(element => {
+        if (element.promedioFinal >= 7) {
+            aprobadosGrupo1 += 1;
+        }
+        else{
+            desaprobadosGrupo1 += 1;
+        }
+    });
+
+    let aprobadosGrupo2 = 0;
+    let desaprobadosGrupo2 = 0;
+    listaGrupo2.forEach(element => {
+        if (element.promedioFinal >= 7) {
+            aprobadosGrupo2 += 1;
+        }
+        else{
+            desaprobadosGrupo2 += 1;
+        }
+    });
+
+    let aprobadosGrupo3 = 0;
+    let desaprobadosGrupo3 = 0;
+    listaGrupo3.forEach(element => {
+        if (element.promedioFinal >= 7) {
+            aprobadosGrupo3 += 1;
+        }
+        else{
+            desaprobadosGrupo3 += 1;
+        }
+    });
+
+    let aprobadosGrupo4 = 0;
+    let desaprobadosGrupo4 = 0;
+    listaGrupo4.forEach(element => {
+        if (element.promedioFinal >= 7) {
+            aprobadosGrupo4 += 1;
+        }
+        else{
+            desaprobadosGrupo4 += 1;
+        }
+    });
+
+    let grupo = identificaciónDeGrupo();
+    switch (grupo) {
+        case 1:
+            mostrarGrafica(aprobadosGrupo1, desaprobadosGrupo1)
+            break;
+        case 2:
+            mostrarGrafica(aprobadosGrupo2, desaprobadosGrupo2)
+            break;
+        case 3:
+            mostrarGrafica(aprobadosGrupo3, desaprobadosGrupo3)
+            break;
+        case 4:
+            mostrarGrafica(aprobadosGrupo4, desaprobadosGrupo4)
+            break;
+        default:
+            break;
+    }
 }
 
 function mostrarPromedios(array){
@@ -325,5 +346,7 @@ function recargarPagina() {
 
 // EVENTOS
 btnGuardar.addEventListener("click", guardarDatos);
-$(".resultados").click(filterArray);
+$(".resultados").click(filtrarArrayPorGrupos);
+$(".mejorAlumno").click(filtrarArrayMejorAlumno);
+$(".grafica").click(filtrarArrayPorAprobacion)
 
